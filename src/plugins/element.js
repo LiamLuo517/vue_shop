ea5be083eeb59157e8c443b5bdff5c7138cc96e0
unknown <xirui.luo.life@gmail.com>
2021-07-19 16:15:30 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// 按需导入
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 把弹框组件挂载到Vue的原型对象上，这样的话，每一个组件都可以直接通过this来访问到 $message ,只要访问到 $message 就可以进行弹窗提示了
+Vue.prototype.$message = Message
