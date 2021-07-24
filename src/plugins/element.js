35c84575f2da813ac53d4a870ab1f0821b9da73f
unknown <xirui.luo.life@gmail.com>
2021-07-24 17:45:24 +0800

    完成用户列表功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
 // 按需导入
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 // 把弹框组件挂载到Vue的原型对象上，这样的话，每一个组件都可以直接通过this来访问到 $message ,只要访问到 $message 就可以进行弹窗提示了
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
